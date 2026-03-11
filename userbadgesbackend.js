5e116eca2595f11dd8655a3e4b2ff4cdc58a79b5
RaptorCoding <146634263+RaptorCoding@users.noreply.github.com>
2026-03-11 12:43:56 +0100

    Create userbadgesbackend.js

---
+++ userbadgesbackend.js
@@ -0,0 +1,65 @@
+window.AVIA_USER_BADGES = [
+
+    {
+        name: "Avia Client Owner",
+        icon: "👑",
+        color: "var(--md-sys-color-primary)",
+        users: [
+            "AvaLilac#7432"
+        ]
+    },
+
+    {
+        name: " Cat Girl",
+        icon: "😸",
+        color: "#ff66cc",
+        users: [
+            "Amelia#2716"
+        ]
+    },
+
+    {
+        name: "KamiNeko!",
+        icon: "🦄",
+        color: "#916acf",
+        users: [
+            "Kaze#6558"
+        ]
+    },
+    
+    {
+        name: "The GXT",
+        icon: "💎",
+        color: "#A40202",
+        users: [
+            "MrGXT_yt#0106"
+        ]
+    },
+
+    {
+        name: "Goat",
+        icon: "🐐",
+        color: "#66399E",
+        users: [
+            "Nath#9230"
+        ]
+    },
+    
+    {
+        name: "bunny",
+        icon: "🐇",
+        color: "#FCE4EC",
+        users: [
+            "amybunnygirl#0122"
+        ]
+    },
+    {
+        name: "raptor",
+        icon: "🐇",
+        color: "#FCE4EC",
+        users: [
+            "raptor#0151"
+        ]
+    }
+
+];
